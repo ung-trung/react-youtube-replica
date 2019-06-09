587cd1cb151e08780ec43baf29e9413ee4644d69
Trung <ungkientrung@gmail.com>
2019-06-09 20:39:30 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchBar from './components/SearchBar';
 import youtube from './components/api/youtube';
 import VideoList from './components/VideoList';
 import VideoDetail from './components/VideoDetail';
+import NavBar from './components/NavBar';
 
 export class App extends Component {
   state = { videos: [], selectedVideo: null };
@@ -27,23 +28,27 @@ export class App extends Component {
 
   render() {
     return (
-      <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            {' '}
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
+      <>
+        {' '}
+        <NavBar />
+        <div className="ui container">
+          <SearchBar onTermSubmit={this.onTermSubmit} />
+          <div className="ui grid">
+            <div className="ui row">
+              {' '}
+              <div className="eleven wide column">
+                <VideoDetail video={this.state.selectedVideo} />
+              </div>
+              <div className="five wide column">
+                <VideoList
+                  onVideoSelect={this.onVideoSelect}
+                  videos={this.state.videos}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="ui top menu">
+      <div className="ui container">
+        <a className="header item" href="/">
+          <div className="ui youtube button">
+            <i className="large youtube icon" /> Youtubee
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
